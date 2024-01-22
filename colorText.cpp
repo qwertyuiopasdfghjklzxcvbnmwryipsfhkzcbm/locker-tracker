e30a2f84dd816911f9a77ef62e90eb5b8d77b0fd
qwertyuiopasdfghjklzxcvbnmwryipsfhkzcbm <157412157+qwertyuiopasdfghjklzxcvbnmwryipsfhkzcbm@users.noreply.github.com>
2024-01-22 14:09:35 -0500

    Updated colorText.cpp
    I have done the line things

--- colorText.cpp
+++ colorText.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include "assets.cpp"
 
-void PrintColor(std::string color, std::string text)
+void PrintColor(std::string color, std::string text) // takes the string text and changes the colour
 {
-    std::cout << color << text << RESET << std::endl;
+    std::cout << color << text << RESET << std::endl; // resets the strings colour, then recieves the text and finaly changes the colour.
 }
