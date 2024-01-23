06e40f5bf222f635aaabf842f2b3d9aa6513a71a
qwertyuiopasdfghjklzxcvbnmwryipsfhkzcbm <157412157+qwertyuiopasdfghjklzxcvbnmwryipsfhkzcbm@users.noreply.github.com>
2024-01-23 13:41:06 -0500

    Update colorText.cpp
    i am finish

--- colorText.cpp
+++ colorText.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include "assets.cpp"
 
-void PrintColor(std::string color, std::string text)
+void PrintColor(std::string color, std::string text) // resets variable, recieves the text, then changes the colour of the corrisponding text
 {
     std::cout << color << text << RESET << std::endl;
 }
